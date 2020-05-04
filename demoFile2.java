f2bf991b080cbe2ae44628f5bcfbfaa356c8ee8e
ChristianRemwood <ChristianRemwood@gmail.com>
2020-05-03 21:35:55 -0600

    initial commit

---
+++ demoFile2.java
@@ -0,0 +1,9 @@
+/* HelloWorld.java
+*/
+//@ishoo (Incorrect Localization) [Program is expected to output 'Hello World' in english, but instead outputs 'こんにちは　世界']
+public class demoFile2
+{
+   public static void main(String[] args) {
+       System.out.println("こんにちは　世界");
+   }
+} 
